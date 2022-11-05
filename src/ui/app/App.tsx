db23a1dc96a8d11d1040b71db6d9cde6cddb0dc9
serIvanich <serivan1108@gmail.com>
2022-11-05 19:02:47 +0200

    add axios, do request, get response

--- src/ui/app/App.tsx
+++ src/ui/app/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { projectApi } from '../../api/project_api'
 import { JobList } from '../components/job_list/JobList'
 import { JobPage } from '../components/job_page/JobPage'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+  
+    projectApi.getJob()
+  }, [])
+
   return (
     <div className='App'>
       <Routes>
